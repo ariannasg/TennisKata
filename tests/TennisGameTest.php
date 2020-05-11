543a51ba3c94198db2248112af85a2eaf53c1c3d
Ari <ariannasg.ldn@gmail.com>
2020-05-11 16:21:49 +0100

    Added simple tests for when server and receiver score points

--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -19,6 +19,48 @@ class TennisGameTest extends TestCase
             $tennisGame->getScore(),
             'When both players have 0pts, then the game score should be: "love:love"'
         );
+    }
 
+    public function testGameScoreWhenTheServerWinsOnePointAndTheReceiverHasZero(): void {
+        $server = new TennisPlayer();
+        $receiver = new TennisPlayer();
+
+        $tennisGame = new TennisGame($server, $receiver);
+        $server->winPoint();
+
+        self::assertEquals(
+            'fifteen:love',
+            $tennisGame->getScore(),
+            'When the server wins one point, then the game score should be: "fifteen:love"'
+        );
+    }
+
+    public function testGameScoreWhenTheReceiverWinsOnePointAndTheServerHasZero(): void {
+        $server = new TennisPlayer();
+        $receiver = new TennisPlayer();
+
+        $tennisGame = new TennisGame($server, $receiver);
+        $receiver->winPoint();
+
+        self::assertEquals(
+            'love:fifteen',
+            $tennisGame->getScore(),
+            'When the receiver wins one point, then the game score should be: "love:fifteen"'
+        );
+    }
+
+    public function testGameScoreWhenBothTheServerAndTheReceiverWinOnePoint(): void {
+        $server = new TennisPlayer();
+        $receiver = new TennisPlayer();
+
+        $tennisGame = new TennisGame($server, $receiver);
+        $server->winPoint();
+        $receiver->winPoint();
+
+        self::assertEquals(
+            'fifteen:fifteen',
+            $tennisGame->getScore(),
+            'When both players win one point, then the game score should be: "fifteen:fifteen"'
+        );
     }
 }
