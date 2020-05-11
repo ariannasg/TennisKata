483b8e81eaf33c67314f9ac33dfcea3bc23a1902
Ari <ariannasg.ldn@gmail.com>
2020-05-11 16:39:58 +0100

    Added data provider for testing game score

--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -8,59 +8,65 @@ use TennisKata\TennisPlayer;
 
 class TennisGameTest extends TestCase
 {
-    public function testGameScoreWhenNoneOfThePlayersHaveScoredAnyPoints(): void {
-        $server = new TennisPlayer();
-        $receiver = new TennisPlayer();
-
-        $tennisGame = new TennisGame($server, $receiver);
-
-        self::assertEquals(
-            'love:love',
-            $tennisGame->getScore(),
-            'When both players have 0pts, then the game score should be: "love:love"'
-        );
+    public function provideGameScoreWhenPlayersWinPoints(): array
+    {
+        return [
+            [0, 0, 'love:love'],
+            [0, 1, 'love:fifteen'],
+            [0, 2, 'love:thirty'],
+            [0, 3, 'love:forty'],
+            [1, 0, 'fifteen:love'],
+            [1, 1, 'fifteen:fifteen'],
+            [1, 2, 'fifteen:thirty'],
+            [1, 3, 'fifteen:forty'],
+            [2, 0, 'thirty:love'],
+            [2, 1, 'thirty:fifteen'],
+            [2, 2, 'thirty:thirty'],
+            [2, 3, 'thirty:forty'],
+            [3, 0, 'forty:love'],
+            [3, 1, 'forty:fifteen'],
+            [3, 2, 'forty:thirty'],
+            [3, 3, 'forty:forty'],
+        ];
     }
 
-    public function testGameScoreWhenTheServerWinsOnePointAndTheReceiverHasZero(): void {
+    /**
+     * @dataProvider provideGameScoreWhenPlayersWinPoints
+     * @param int $serverPointsWon
+     * @param int $receiverPointsWon
+     * @param string $expectedGameScore
+     */
+    public function testGameScoreWhenPlayersWinPoints(
+        int $serverPointsWon,
+        int $receiverPointsWon,
+        string $expectedGameScore
+    ): void
+    {
         $server = new TennisPlayer();
         $receiver = new TennisPlayer();
 
         $tennisGame = new TennisGame($server, $receiver);
-        $server->winPoint();
+        for ($i = 1; $i <= $serverPointsWon; $i++) {
+            $server->winPoint();
+        }
 
-        self::assertEquals(
-            'fifteen:love',
-            $tennisGame->getScore(),
-            'When the server wins one point, then the game score should be: "fifteen:love"'
+        for ($i = 1; $i <= $receiverPointsWon; $i++) {
+            $receiver->winPoint();
+        }
+
+        $msg = sprintf(
+            'When server wins %d %s and receiver wins %d %s, then the game score should be: "%s"',
+            $serverPointsWon,
+            $serverPointsWon === 1 ? 'point' : 'points',
+            $receiverPointsWon,
+            $receiverPointsWon === 1 ? 'point' : 'points',
+            $expectedGameScore
         );
-    }
-
-    public function testGameScoreWhenTheReceiverWinsOnePointAndTheServerHasZero(): void {
-        $server = new TennisPlayer();
-        $receiver = new TennisPlayer();
-
-        $tennisGame = new TennisGame($server, $receiver);
-        $receiver->winPoint();
 
         self::assertEquals(
-            'love:fifteen',
+            $expectedGameScore,
             $tennisGame->getScore(),
-            'When the receiver wins one point, then the game score should be: "love:fifteen"'
-        );
-    }
-
-    public function testGameScoreWhenBothTheServerAndTheReceiverWinOnePoint(): void {
-        $server = new TennisPlayer();
-        $receiver = new TennisPlayer();
-
-        $tennisGame = new TennisGame($server, $receiver);
-        $server->winPoint();
-        $receiver->winPoint();
-
-        self::assertEquals(
-            'fifteen:fifteen',
-            $tennisGame->getScore(),
-            'When both players win one point, then the game score should be: "fifteen:fifteen"'
+            $msg
         );
     }
 }
