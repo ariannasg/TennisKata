40cffdcffee0b69e2d93f53af145985aadb004ef
Ari <ariannasg.ldn@gmail.com>
2020-05-11 12:24:19 +0100

    Added dummy test for trying out PHPUnit config

---
+++ tests/TennisScorerTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace TennisGame\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TennisScorerTest extends TestCase
+{
+    public function testScorePoints(): void
+    {
+        self::assertTrue(true);
+    }
+}
